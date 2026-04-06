45298a9dcf67e23c04d41649072f42ce3c1be704
rafid ihsan <rafidn125@gmail.com>
2026-04-06 16:37:36 +0700

    tes css

--- api/page/telurmentah.php
+++ api/page/telurmentah.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/css/stylepage.css">
+    <link rel="stylesheet" href="/css/stylepage1.css">
 </head>
 <style>
     body {
